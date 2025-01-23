91840fd512b01d3ffeb10f896c79444a3647b9cc
norahkerendian <nokerendian@ucsd.edu>
2025-01-23 13:24:25 -0800

    step 3 done

--- global.js
+++ global.js
@@ -4,10 +4,43 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a");
+// const navLinks = $$("nav a");
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-  );
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
 
-  currentLink?.classList.add('current');
+//   currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/norahkerendian', title: 'GitHub' },
+    { url: 'resume/', title: 'Resume' },
+  ];
+  
+  let nav = document.createElement('nav');
+  document.body.prepend(nav);
+  
+  const ARE_WE_HOME = document.documentElement.classList.contains('home');
+  
+  for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+  
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+  
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+      }
+
+    if (a.host !== location.host) {
+        a.target = '_blank';
+    }
+  }
