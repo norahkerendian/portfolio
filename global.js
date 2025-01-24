8384a4a3ac5f65664d468a14a78965ceee396d1b
norahkerendian <nokerendian@ucsd.edu>
2025-01-23 20:33:07 -0800

    step 4 done

--- global.js
+++ global.js
@@ -44,3 +44,36 @@ let pages = [
         a.target = '_blank';
     }
   }
+
+document.body.insertAdjacentHTML(
+'afterbegin',
+`
+<label class="color-scheme">
+    Theme:
+    <select>
+    <option value="auto">Automatic</option>
+    <option value="light">Light</option>
+    <option value="dark">Dark</option>
+    </select>
+</label>`
+);
+
+function setColorScheme(colorScheme) {
+    document.documentElement.style.setProperty('color-scheme', colorScheme);
+    localStorage.colorScheme = colorScheme;
+  }
+  
+const select = document.querySelector(".color-scheme select");
+  
+if (localStorage.colorScheme) {
+    setColorScheme(localStorage.colorScheme); 
+    select.value = localStorage.colorScheme; 
+  } else {
+    setColorScheme('auto'); 
+    select.value = 'auto'; 
+  }
+
+select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    setColorScheme(event.target.value);
+  });
