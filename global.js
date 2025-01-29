0ee4812b13dc9f7c36be03d7412fd977cbf45627
norahkerendian <nokerendian@ucsd.edu>
2025-01-29 01:22:24 -0800

    step 3 done

--- global.js
+++ global.js
@@ -129,3 +129,8 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
   });
 
 }
+
+export async function fetchGitHubData(username) {
+  // return statement here
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
