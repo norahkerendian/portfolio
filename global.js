7d69018b780ad8ba0769c585c8e36e558aaec0cf
norahkerendian <nokerendian@ucsd.edu>
2025-01-28 22:58:07 -0800

    debugging

--- global.js
+++ global.js
@@ -94,3 +94,39 @@ form?.addEventListener('submit', function (event) {
 
     location.href = url;
 });
+
+export async function fetchJSON(url) {
+  try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+      if (!response.ok) {
+        console.log('response')
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        
+    }
+      const data = await response.json();
+    return data; 
+
+
+  } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+fetchJSON('../lib/projects.json').then(data => console.log(data));
+
+// export async function fetchJSON(url) {
+//   try {
+//       console.log(`Fetching: ${url}`); // Debug log
+//       const response = await fetch(url);
+//       if (!response.ok) {
+//         throw new Error(`Failed to fetch projects: ${response.statusText}`);
+//       }
+//       const data = await response.json();
+//       return data; 
+//   } catch (error) {
+//       console.error('Error fetching or parsing JSON data:', error);
+//   }
+// }
+
+// fetchJSON('lib/projects.json').then(data => console.log('Fetched Data:', data));
