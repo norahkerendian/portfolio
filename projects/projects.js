362d7eab2e26293b7ef66ddf71aa5d7a1fc973b4
norahkerendian <nokerendian@ucsd.edu>
2025-02-02 00:39:39 -0800

    step 2 done

--- projects/projects.js
+++ projects/projects.js
@@ -24,9 +24,16 @@ let arc = arcGenerator({
 
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+    { value: 1, label: 'apples' },
+    { value: 2, label: 'oranges' },
+    { value: 3, label: 'mangos' },
+    { value: 4, label: 'pears' },
+    { value: 5, label: 'limes' },
+    { value: 5, label: 'cherries' },
+  ];
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 
@@ -35,4 +42,11 @@ arcs.forEach((arc, idx) => {
       .append('path')
       .attr('d', arc)
       .attr('fill', colors(idx)) 
+})
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+    legend.append('li')
+          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
 })
