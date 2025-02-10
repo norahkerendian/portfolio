e43a8654e1d304bbade3e0a37c3f93cebfb9e5b5
norahkerendian <nokerendian@ucsd.edu>
2025-02-09 23:50:46 -0800

    step 1.2 done

---
+++ meta/main.js
@@ -0,0 +1,49 @@
+let data = [];
+let commits = [];
+
+async function loadData() {
+    data = await d3.csv('loc.csv', (row) => ({
+      ...row,
+      line: Number(row.line), // or just +row.line
+      depth: Number(row.depth),
+      length: Number(row.length),
+      date: new Date(row.date + 'T00:00' + row.timezone),
+      datetime: new Date(row.datetime),
+    }));
+
+    processCommits();
+    console.log(commits);
+  }
+
+function processCommits() {
+    commits = d3
+      .groups(data, (d) => d.commit)
+      .map(([commit, lines]) => {
+        let first = lines[0];
+        let { author, date, time, timezone, datetime } = first;
+        let ret = {
+          id: commit,
+          url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+          author,
+          date,
+          time,
+          timezone,
+          datetime,
+          hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+          totalLines: lines.length,
+        };
+  
+        Object.defineProperty(ret, 'lines', {
+          value: lines,
+          configurable: false,
+          writable: false,
+          enumerable: false,
+        });
+  
+        return ret;
+      });
+  }
+
+  document.addEventListener('DOMContentLoaded', async () => {
+    await loadData();
+  });
